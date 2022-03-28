bc7b7addd66a3e244e5a47f13d3f73974caa1b19
Hlib <gleb.shipula@gmail.com>
2022-03-28 17:26:48 +0300

    updates 28.03.2022

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const svinosobaki = {
-  "planes": 121,
+  "planes": 123,
   "helicopters": 127,
-  "cannons": 294,
-  "grads": 93,
-  "drones": 50,
+  "cannons": 302,
+  "grads": 95,
+  "drones": 66,
   "ships": 7,
-  "ppo": 56,
-  "tanks": 582,
-  "bmp": 1664,
-  "trucks": 1144,
+  "ppo": 54,
+  "tanks": 586,
+  "bmp": 1694,
+  "trucks": 1150,
   "tankers": 73,
-  "soldiers": 16600
+  "soldiers": 17000
 }
 
 for (const key in svinosobaki) {
